e51ec7b1d0bf9bab1f3f420d7a9df12f0928c9d6
Emirhan Eren <emirhanneren52@gmail.com>
2022-03-27 18:32:44 +0300

    27.03.2022

--- OtelProject-Mvc/Controllers/MisafirController.cs
+++ OtelProject-Mvc/Controllers/MisafirController.cs
@@ -3,25 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using OtelProject_Mvc.Models.Entity;
 
 namespace OtelProject_Mvc.Controllers
 {
+    //Giriş Yapılmadan panele yönlendirme olmaması için controller seviseyine taşınır
+    //Web config üzerinden loginurl'e yönlendirmenin yapılacak olduğu konum yazılır
+    [Authorize]
+
     public class MisafirController : Controller
     {
-
+        
         DbOtelEntities db = new DbOtelEntities();
 
         public ActionResult Index()
         {
-            return View();
+            var misafirMail = (string)Session["Mail"];
+            var misafirBilgi = db.TblYeniKayit.Where(x => x.Mail == misafirMail).FirstOrDefault();
+            return View(misafirBilgi);
         }
 
         public ActionResult Rezervasyonlarim()
         {
+            //Oturumdaki kullanıcının mail adresini alma
+            var misafirMail = (string)Session["Mail"];
+            //ViewBag.a = misafirMail;
+            
+            var misafirId = db.TblYeniKayit.Where(x => x.Mail == misafirMail).Select(y => y.ID).FirstOrDefault();
+            
             //Hangi misafirin değerlerinin getiriileceği
-            var degerler = db.TblRezervasyon.Where(x => x.Misafir == 1).ToList();
+            var degerler = db.TblRezervasyon.Where(x => x.Misafir == misafirId).ToList();
             return View(degerler);
+        }
+
+        public ActionResult MisafirBilgiGuncelle(TblYeniKayit p)
+        {
+            var misafir = db.TblYeniKayit.Find(p.ID);
+            misafir.AdSoyad = p.AdSoyad;
+            misafir.Sifre = p.Sifre;
+            misafir.Telefon = p.Telefon;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return RedirectToAction("Index","AnaSayfa");  
         }
     }
 }
